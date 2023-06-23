3e5ab6f3125ace36011f93da5df9d7f3a29f121d
junhyukM <minjh1031@gmail.com>
2023-06-23 17:51:36 +0900

    update

--- swea/queue/5102_노드의거리/sol_swcamp.py
+++ swea/queue/5102_노드의거리/sol_swcamp.py
@@ -4,23 +4,27 @@ sys.stdin = open('input.txt')
 T = int(input())
 
 for tc in range(1, T+1):
-    V, E = map(int, input().split())
+    # V : 노드수, E : 간선수
+    V, E = list(map(int, input().split()))
+
     nodes = [[0] * (V+1) for _ in range(V+1)]
-    
+
     for line in range(E):
         start, end = list(map(int, input().split()))
-        # 양쪽 방향으로 다 오갈수 있어서 대각선을 기준으로 대칭인 형태
         nodes[start][end] = 1
         nodes[end][start] = 1
 
+    # pprint(nodes)
+
+
     # S : 확인을 해야하는 출발 노드
     # G : 연결되어있음을 확인하는 도착노드
-    S, G = map(int, input().split())
-
+    S, G = list(map(int, input().split()))
+    
     # 방문 체크리스트
     check_list = [False] * (V+1)
     # 거리계산을 위한 리스트
-    distance = [0] * (V+1)
+    distance = [0] * (V+1)  
     # bfs 처리용 queue
     queue = []
 
@@ -34,14 +38,14 @@ for tc in range(1, T+1):
         v = queue.pop(0)
         # v와 연결된 노드 찾기
         for w in range(V+1):
-            # v와 연결된 w 찾기
+            # v와 연결된 w
             if nodes[v][w] == 1:
                 # 만약 아직 방문하지 않았다면
                 if check_list[w] == False:
                     # 방문 처리
-                    check_list[w] == True
+                    check_list[w] = True
                     # 현재 깊이에서 1 증가 시킨 후 다음 위치의 깊이 저장
-                    distance[w] = distance[v] + 1 
+                    distance[w] = distance[v] + 1
                     queue.append(w)
 
     print(distance[G])
